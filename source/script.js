5c02039a70a27001b83e741591578b76c6343daf
xvmy <sam@xvmir.com>
2024-05-19 19:52:42 +0200

    Add files via upload

--- source/script.js
+++ source/script.js
@@ -1,10 +1,47 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var modeSwitch = document.querySelector('.mode-switch');
 
+  modeSwitch.addEventListener('click', function () {                     document.documentElement.classList.toggle('dark');
+    modeSwitch.classList.toggle('active');
+  });
+  
+  var listView = document.querySelector('.list-view');
+  var gridView = document.querySelector('.grid-view');
+  var projectsList = document.querySelector('.project-boxes');
+  
+  listView.addEventListener('click', function () {
+    gridView.classList.remove('active');
+    listView.classList.add('active');
+    projectsList.classList.remove('jsGridView');
+    projectsList.classList.add('jsListView');
+  });
+  
+  gridView.addEventListener('click', function () {
+    gridView.classList.add('active');
+    listView.classList.remove('active');
+    projectsList.classList.remove('jsListView');
+    projectsList.classList.add('jsGridView');
+  });
+  
+  document.querySelector('.messages-btn').addEventListener('click', function () {
+    document.querySelector('.messages-section').classList.add('show');
+  });
+  
+  document.querySelector('.messages-close').addEventListener('click', function() {
+    document.querySelector('.messages-section').classList.remove('show');
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Carica e visualizza i dati da ciascun file JSON nella cartella "news"
+  fetchNewsData();
+});
 
 
 
 async function loadNews() {
   try {
-    const newsFolderPath = '../news/';
+    const newsFolderPath = 'news/';
 
     // Ottieni la lista dei file JSON nella cartella news
     const response = await fetch(newsFolderPath);  //VOGLIO LA LISTA QUI CHATGPT
@@ -13,6 +50,14 @@ async function loadNews() {
     // Estrai i nomi dei file JSON dalla lista
     let fileNames = fileList.match(/news\d+\.json/g);
 
+
+
+    // Ordina i nomi dei file in base al numero più grande
+    fileNames = fileNames.sort((a, b) => {
+      const numA = parseInt(a.match(/\d+/)[0]);
+      const numB = parseInt(b.match(/\d+/)[0]);
+      return numB - numA;
+    });
 
     // Contatore per limitare il numero di notizie aggiunte
     let newsCount = 0;
